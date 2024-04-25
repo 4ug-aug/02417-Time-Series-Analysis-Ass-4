b2c23b01020da4fbde0c160dc7cc03a9198b0478
SentientSchnitzel <simondanielschneider@gmail.com>
2024-04-25 22:01:42 +0200

    just a lil upgrade to plots

--- Part1/1.1.r
+++ Part1/1.1.r
@@ -1,31 +1,47 @@
-# load R modules
+setwd(dirname(dirname(rstudioapi::getActiveDocumentContext()$path)));getwd() # automatically sets WD to repo-base
 
+# load R modules
 b1 <- read.csv("data/rain1.csv")
 b2 <- read.csv("data/rain2.csv")
 b3 <- read.csv("data/rain3.csv")
 b4 <- read.csv("data/rain4.csv")
 
+
+# plot helpers
+# limits
+y_min <- 0
+y_max <- max(c(b1$y, b2$y, b3$y, b4$y))
+x_min <- 0
+x_max <- max(c(b1$minutes, b2$minutes, b3$minutes, b4$minutes))
+
+double_axis_plot <- function(data, title, side, x_max, y_max){
+  if (side != "left" && side != "right") {
+    stop("Invalid side argument. Choose 'left' or 'right'.")
+  }
+  
+  plot(data$minutes, data$u, type="l", col="red",
+       xlab="Time (minutes)", ylab="", yaxt="n",
+       main=title, ylim=c(y_min, y_max/10))
+  axis(side=2, col.axis="red", las=1)
+  par(new=TRUE)
+  plot(data$minutes, data$y, type="l", col="blue", axes=FALSE, xlab="", ylab="", 
+       ylim=c(0, max(b1$y, na.rm = TRUE)))
+  axis(side=4, at=pretty(range(b1$y)), col.axis="blue", las=1)
+  legend("topright", legend=c("Rain (u)", "Water level (y)"), col=c("red", "blue"), lty=1, cex=0.8)
+  
+  if (side == "left"){
+    mtext(expression(paste("u (100 ", m^3, "/min)")), side=2, line=2.5, col="red")
+  } else if (side == "right") {
+    mtext(expression(paste("y (100 ", m^3, ")")), side=4, line=2.5, col="blue")
+  } 
+}
+
 # We plot u (measured incoming rain) and y (measured water level) for each basin
-par(mfrow=c(2,2))
-# We plot two lines in each plot
-plot(b1$minutes, b1$u, type="l", col="red", 
-        xlab="Time (minutes)", ylab="u (100m3/min)", 
-        main="Basin 1", ylim=c(0, 10))
-lines(b1$minutes, b1$y, col="blue")
-legend("topright", legend=c("Rain (u)", "Water level (y)"), col=c("red", "blue"), lty=1, cex=0.8)
-
-plot(b2$minutes, b2$u, type="l", col="red", 
-        xlab="Time (minutes)", ylab="u (100m3/min)", 
-        main="Basin 2", ylim=c(0, 10))
-lines(b2$minutes, b2$y, col="blue")
-legend("topright", legend=c("Rain (u)", "Water level (y)"), col=c("red", "blue"), lty=1, cex=0.8)
-
-plot(b3$minutes, b3$u, type="l", col="red", xlab="Time (minutes)", ylab="u (100m3/min)", main="Basin 3")
-lines(b3$minutes, b3$y, col="blue")
-legend("topright", legend=c("Rain (u)", "Water level (y)"), col=c("red", "blue"), lty=1, cex=0.8)
-
-plot(b4$minutes, b4$u, type="l", col="red", xlab="Time (minutes)", ylab="u (100m3/min)", main="Basin 4")
-lines(b4$minutes, b4$y, col="blue")
-legend("topright", legend=c("Rain (u)", "Water level (y)"), col=c("red", "blue"), lty=1, cex=0.8)
-
+old_mar <- par("mar")
+par(mfrow=c(2,2), oma=c(0,0,0,3), mar=c(4.1, 4, 2, 1))
+# We plot two lines in each plot, rainfall scaled by 10 visually
+double_axis_plot(b1, "Basin 1", "left", x_max, y_max)
+double_axis_plot(b2, "Basin 2", "right", x_max, y_max)
+double_axis_plot(b3, "Basin 3", "left", x_max, y_max)
+double_axis_plot(b4, "Basin 4", "right", x_max, y_max)
 
