efc6604c8cd0b679fe56ba53a5c5b3d9e77c706c
SentientSchnitzel <simondanielschneider@gmail.com>
2024-05-03 14:08:03 +0200

    kom desværre ikke såp meget videre i dag

--- Part2/2_4.R
+++ Part2/2_4.R
@@ -1,12 +1,6 @@
 setwd(dirname(dirname(rstudioapi::getActiveDocumentContext()$path)));getwd() # automatically sets WD to repo-base
 
 b1 <- read.csv("data/rain1.csv")
-b2 <- read.csv("data/rain2.csv")
-b3 <- read.csv("data/rain3.csv")
-b4 <- read.csv("data/rain4.csv")
-
-length(b1$minutes)
-
 
 # parameters
 a <- 0.04
@@ -20,24 +14,38 @@ N <- length(b1$minutes) # dependent on what experiment we use
 X <- matrix(nrow=K_states,ncol=N)
 X0 <- c(0,0,0,0) # column vector
 X[,1] <- X0
-
-A <- matrix(c(1-a,0,0,0,
-              a,1-a,0,0,
-              0,a,1-a,0,
-              0,0,a,0.98),nrow=K_states, byrow=TRUE)
 B <- matrix(c(1,0,0,0),nrow=K_states) # column
 C <- matrix(c(0,0,0,1),nrow=1) # row 
-Sigma1 <- diag(K_states)*sigma1
-Sigma2 <- matrix(sigma2)
 
-Y <- numeric(N)
-Y[1] <- C%*%X[,1] + sqrt(Sigma2) %*% rnorm(1) # scalar (the level of water in basin)
 
-# running N steps Kalman filtering with input u_t (rainfall)
-for (I in 2:N){
-  X[,I] <- A %*% X[,I-1,drop=FALSE] + B%*%b1$u[I-1] + chol(Sigma1) %*% matrix(rnorm(K_states),ncol=1)
-  Y[I] <- C %*% X[,I] + sqrt(Sigma2) %*% rnorm(1)
+kalman_filter <- function(input, a, sigma1, sigma2){
+  A <- matrix(c(1-a,0,0,0,
+                a,1-a,0,0,
+                0,a,1-a,0,
+                0,0,a,0.98),nrow=4, byrow=TRUE)
+  Sigma1 <- diag(4)*sigma1
+  Sigma2 <- matrix(sigma2)
+  Y <- numeric(N)
+  Y[1] <- C%*%X[,1] + sqrt(Sigma2) %*% rnorm(1) # scalar (the level of water in basin)
+  # running N steps Kalman filtering with input u_t (rainfall)
+  for (I in 2:N){
+    X[,I] <- A %*% X[,I-1,drop=FALSE] + B%*%input[I-1] + chol(Sigma1) %*% matrix(rnorm(4),ncol=1)
+    Y[I] <- C %*% X[,I] + sqrt(Sigma2) %*% rnorm(1)
+  }
+  return(list(X = X, Y = Y))
 }
+
+simul <- kalman_filter(b1$u, a, sigma1, sigma2)
+X <- simul$X
+Y <- simul$y
+
+
+### TODO: MAKE MANY PLOTS WITH DIFFERENT PARAMETER VALUES in kalman_filter. see 1_1 with more compact plotting
+
+
+
+
+
 
 ### plotting
 y_max <- max(b1$y)
